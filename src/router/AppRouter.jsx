13774634a1681d7bac797bc8bcd643c4cfea19f9
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-13 12:26:07 -0300

    init

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        {/* //Login y registro*/}
+        <Route />
+        {/* JournalApp */}
+        <Route />
+      </Routes>
+    </>
+  );
+};
