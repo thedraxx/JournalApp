250caf4b80db952fe7a5702a8842fcda4a10b0f6
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-14 16:27:49 -0300

    added more styles to loginPage with MaterialUI, added router

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 import React from "react";
 
 export const LoginPage = () => {
@@ -43,6 +45,28 @@ export const LoginPage = () => {
                 placeholder="contrasena"
                 fullWidth
               />
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="end">
+              <Link
+                component={RouterLink} // RouterLink es link de react-router-dom solo que se cambio el nombre para no entrar en conflicto con materialUI
+                color={"inherit"}
+                to="/auth/register"
+              >
+                Crea una cuenta
+              </Link>
             </Grid>
           </Grid>
         </form>
