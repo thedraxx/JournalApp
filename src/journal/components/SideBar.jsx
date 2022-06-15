4592224ff7ea5af960c970a025db2b1ce3bbd50b
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-15 13:33:26 -0300

    add SideBar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,55 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component={"nav"}
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent" //temporary
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component={"div"}>
+            Frank Miller
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Inicio", "Entradas", "Salidas", "Reportes"].map((text) => (
+            <ListItem button key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText secondary={"lorem"} />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,17 @@
 import { Box } from "@mui/system";
 import React from "react";
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
+  //El children viene de JournalPage.jsx y es una función que retorna un componente.
   return (
     <Box sx={{ display: "flex" }}>
       JournalLayout
-      {/* navbar drawerWidth*/}
+      {/* Llamamos Barra de navegacion y SideBar */}
       <NavBar drawerWidth={drawerWidth} />
-      {/* sidebar drawerWidth*/}
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
         {children}
