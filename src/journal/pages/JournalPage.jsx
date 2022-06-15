6309feae422e7bfd634f367c4a1e8c8a8edb02b8
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-15 14:23:16 -0300

    added NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     // Enviamos el children a JournalLayout para que lo renderice.
     <JournalLayout>
-      <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
