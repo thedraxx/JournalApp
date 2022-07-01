6d1dc81491d15540cb7da9d3dd67f0122249d2d1
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-01 15:16:29 -0300

    added checkingAuth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,16 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+  // Tomamos el estado de autenticación del store
+  const { status } = useSelector((state) => state.auth);
+  // Si estamos checkando el auth, mostramos el componente de carga
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <BrowserRouter>
       <Routes>
---
+++ src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,24 @@
+import { CircularProgress, Grid } from "@mui/material";
+import React from "react";
+
+export const CheckingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        //Extra styling
+        minHeight: "100vh",
+        backgroundColor: "primary.main", // Viene de AppTheme
+        padding: 4,
+      }}
+    >
+      <Grid item direction="row" justifyContent="center">
+        <CircularProgress color="warning" />
+      </Grid>
+    </Grid>
+  );
+};
