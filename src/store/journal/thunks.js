69ba32e901ffae2f9a9069c624889ea76db662e1
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-05 14:16:42 -0300

    init thunks journal

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,17 @@
+
+
+export const startNewNote = () => {
+    return async (dispatch) => {
+        // uid 
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+
+        // dispatch()
+        // dispatch(newNote)
+        // dispatch(activarNote)
+
+    }
+}
