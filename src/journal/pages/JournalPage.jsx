9f629b2ffa4843d441b0434e544b22f7fdeb39b8
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-05 16:33:13 -0300

    create new note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,8 +3,19 @@ import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPage = () => {
+  // Usamos useDispatch para poder usar el dispatch de Redux
+  const dispatch = useDispatch();
+
+  // Mandamos a llamar al dispatch para que cree una nueva nota
+  // al ser algo asíncrono, llamamos al thunk
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     // Enviamos el children a JournalLayout para que lo renderice.
     <JournalLayout>
@@ -13,6 +24,7 @@ export const JournalPage = () => {
 
       {/* Boton para agregar notas */}
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           //Usando SX tenemos acceso al tema global y por eos podemos usar el color que viene del theme
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,13 +1,22 @@
-
-
+import { collection, doc, setDoc } from 'firebase/firestore/lite'
+import { FirebaseDB } from '../../firebase/Config';
 export const startNewNote = () => {
-    return async (dispatch) => {
+
+    return async (dispatch, getState) => {
+        // Usamos getState para obtener el estado actual
+        const { uid } = getState().auth;
+
         // uid 
         const newNote = {
             title: '',
             body: '',
             date: new Date().getTime(),
         }
+
+        // Aca insertamos el nuevo documento
+        const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+        await setDoc(newDoc, newNote);
+
 
         // dispatch()
         // dispatch(newNote)
